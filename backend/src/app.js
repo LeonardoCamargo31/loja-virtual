b6150b71c81d2c92355d2b2ab97f56902e711a89
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2019-03-30 15:41:13 -0300

    Adicionando cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,10 +4,11 @@ require("dotenv").config({
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app=express()//inicializando nossa aplicação
 
-
+app.use(cors())
 app.use(bodyParser.json())//para ele entender quando enviar uma requisão com informações em json
 app.use(bodyParser.urlencoded({extended:false}))//para ele entender quando passar parametros via url
 
